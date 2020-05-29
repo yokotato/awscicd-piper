a4594cca79846873fd4bf52d7a5a43ba2db9099d
yokotato <yokotato@yahoo.co.jp>
2020-05-29 13:33:06 +0900

    AWS

--- web/app.py
+++ web/app.py
@@ -9,7 +9,7 @@ def mainmenu():
     return """
     <html>
     <body>
-    <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
+    <center><h1>Hello こんにちは</h1><br/>
     </body>
     </html>"""
 
